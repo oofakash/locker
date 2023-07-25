6bdcc6bd94b043f6dc2bf180c21da6c9a0a03113
Akash <akash_sharma4851@hotmail.com>
2023-07-25 19:07:51 +0530

    added validation. dir check fn & changed log file creation

--- src/main/utils/fileOperations.java
+++ src/main/utils/fileOperations.java
@@ -7,26 +7,43 @@ import java.io.*;
 public class fileOperations {
     public static String getPassword() {
         try {
-            File passwordDir = new File("E:/Web Projects/Password");
-            if(passwordDir.exists()) {
-                File passwordFile = new File("E:/Web Projects/Password/password.log");
+            File passwordFile = new File("E:/Web Projects/LockerDir/password.log");
+            if(passwordFile.exists()) {
                 ObjectInputStream in = new ObjectInputStream(new FileInputStream(passwordFile));
                 password password = (password) in.readObject();
                 return password.getPassword();
             } else {
-                passwordDir.mkdir();
-                File passwordFile = new File("E:/Web Projects/Password/password.log");
                 passwordFile.createNewFile();
                 password password = new password("akash");
                 ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(passwordFile));
                 out.writeObject(password);
                 return password.getPassword();
-
-
             }
         } catch (Exception ignored) {
 
         }
         return null;
+    }
+
+    public static Boolean validateFile(String filePath) {
+        if (filePath == null || filePath.isEmpty()) {
+            return false;
+        }
+        File file = new File(filePath);
+        if (file.exists() && !file.isDirectory()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void checkAppDirectory() {
+        try {
+            File appDir = new File("E:/Web Projects/LockerDir");
+            if (!appDir.exists()) {
+                appDir.mkdir();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
